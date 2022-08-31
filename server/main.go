3b34ca02a2aad6be0a1de5f856dc28588922e2c8
NoamBoni <noamboni55@gmail.com>
2022-08-31 15:55:17 +0300

    moved to go + dockerizing

--- server/main.go
+++ server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
-func main(){
-	fmt.Println("Hellooooooooooooooooooo")
+	"github.com/NoamBoni/bitly/server/helpers"
+	"github.com/NoamBoni/bitly/server/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	helpers.LoadEnv()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		user := models.User{
+			Firstname: "noam",
+			Lastname:  "boni",
+			Email:     "e@io.io",
+			Password:  "123456",
+		}
+		user.Insert()
+		c.JSON(http.StatusOK, gin.H{
+			"message": "noam",
+			"user":    user,
+		})
+	})
+	r.Run()
 }
---
+++ server/sql/init.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE bitly;
+GRANT ALL PRIVILEGES ON DATABASE bitly TO postgres;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    email VARCHAR(50),
+    password VARCHAR(100)
+);
+
+CREATE TABLE urls (
+    id SERIAL PRIMARY KEY,
+    original_url VARCHAR(100),
+    modified_url VARCHAR(100),
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
